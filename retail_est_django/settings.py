21dffc8d1050b74b2de2447647de0ad6a233f832
Suresh J <63852803+sureshtechi@users.noreply.github.com>
2022-07-04 14:12:29 +0530

    Update settings.py

--- retail_est_django/settings.py
+++ retail_est_django/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'retail_est_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
