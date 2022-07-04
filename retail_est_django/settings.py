e86b85d9fe650a062f82bf896553b72d5cedb4a7
Suresh J <63852803+sureshtechi@users.noreply.github.com>
2022-07-04 12:58:27 +0530

    deploy-changes

--- retail_est_django/settings.py
+++ retail_est_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=)qh_@(@+ny_r-af1evut7ewuv_17)mnxc$k-@*bo%b0cjsf1m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 LANGUAGE_CODE = 'en-us'
 
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
